803ccb06daae326557ff9ee47f7c7a95a0fc7ba9
aryapradeep1 <aaryapradeep04@gmail.com>
2026-07-10 12:24:27 +0530

    delete car completed

---
+++ src/components/DeleteCar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const DeleteCar = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                             <label htmlFor="" className="form-label">Enter Car id</label>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            
+                            <button className="btn-btn-success">Delete</button>
+
+                             </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default DeleteCar
